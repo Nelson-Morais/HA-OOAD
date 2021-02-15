05236a854229aaf2e3b8a6b2ec46a918c5d0d4c3
Lokaler Administrator <skylion.scp@gmail.com>
2021-02-15 19:44:00 +0100

    OfferList done

--- bazaar/models.py
+++ bazaar/models.py
@@ -1,21 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
-class Offer(models.Model):
+class OfferModel(models.Model):
     userowner_id = models.IntegerField(default=0)
     title = models.CharField(max_length=512)
     description = models.CharField(max_length=512)
     is_deleted = models.BooleanField(default=False)
 
 
-class Request(models.Model):
-
+class RequestModel(models.Model):
     userowner_id = models.IntegerField()
     offer_id = models.IntegerField(default=0)
     text = models.CharField(max_length=512)
     RequestStatus = (
-        ('1','open'),
-        ('2','accepted'),
-        ('3','deleted')
+        ('1', 'open'),
+        ('2', 'accepted'),
+        ('3', 'deleted')
     )
-    status = models.CharField(max_length=1,choices=RequestStatus, default=1)
+    status = models.CharField(max_length=1, choices=RequestStatus, default=1)
