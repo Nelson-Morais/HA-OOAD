7a9e735c08267871510f0f08a2b63685cc8016cb
Kevin Lucas Simon <brrloop@live.de>
2021-02-15 20:05:27 +0100

    Developing Notification Center +Added: Message Model in Notification +Updated: Messages only load when user is authenticated +Updated: Notification View Functions +Removed: Module Actions +Removed: Messages Class in Models +Fix: Messages Loading Bug Info: Migration needed. Bug: Log escalates sometimes, but Server ist fine...

--- ShareCare/urls.py
+++ ShareCare/urls.py
@@ -38,7 +38,7 @@ urlpatterns = [
     # Personal Section
     path("me/offer/", get_personal_offer_list, name="personal_offer_list"),
     path("me/request/", get_personal_request_list, name="personal_request_list"),
-    path("me/notifications/", MessageView.get_notifications, name="notification_list"),
+    path("me/notifications/", get_notifications, name="notification_list"),
 
     # UserAuth
     path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
